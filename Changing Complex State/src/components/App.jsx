fff1a57488da6c26e69ac56b4d129f394fb980cd
Avinash Dhauni <avinashdhauni96@gmail.com>
2022-08-09 20:52:14 +0530

    changed App.jsx code and used Spread Operator

--- Changing Complex State/src/components/App.jsx
+++ Changing Complex State/src/components/App.jsx
@@ -9,27 +9,36 @@ function App() {
 
   function handleChange(event) {
     const { name, value } = event.target;
+    //***********Without using Spread Operator************* */
 
+    // setContact((prevValue) => {
+    //   if (name === "fName") {
+    //     return {
+    //       fName: value,
+    //       lName: prevValue.lName,
+    //       email: prevValue.email
+    //     };
+    //   } else if (name === "lName") {
+    //     return {
+    //       fName: prevValue.fName,
+    //       lName: value,
+    //       email: prevValue.email
+    //     };
+    //   } else if (name === "email") {
+    //     return {
+    //       fName: prevValue.fName,
+    //       lName: prevValue.lName,
+    //       email: value
+    //     };
+    //   }
+    // });
+
+    //****************** */ Using Spread Operator*************
     setContact((prevValue) => {
-      if (name === "fName") {
-        return {
-          fName: value,
-          lName: prevValue.lName,
-          email: prevValue.email
-        };
-      } else if (name === "lName") {
-        return {
-          fName: prevValue.fName,
-          lName: value,
-          email: prevValue.email
-        };
-      } else if (name === "email") {
-        return {
-          fName: prevValue.fName,
-          lName: prevValue.lName,
-          email: value
-        };
-      }
+      return {
+        ...prevValue,
+        [name]: value
+      };
     });
   }
   return (
